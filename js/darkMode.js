aaacc2a8b5917d7b9e1a8317105246c064d51192
Nathan Roberts <harry4234@hotmail.co.uk>
2023-02-22 18:26:00 +0000

    First Push
    A ecommerce design for a clothing site, non functional but can be expanded on.

---
+++ js/darkMode.js
@@ -0,0 +1,23 @@
+var darkMode = localStorage.getItem('darkMode');
+var lightSwitch = document.querySelector('.nav_bar__toggle_colour img');
+var mainLogo = document.querySelector('.nav_bar__logo img');
+
+if (darkMode == 'true') turnOffLights();
+
+function turnOffLights() {
+	lightSwitch.setAttribute('onClick', 'javascript: turnOnLights();');
+	localStorage.setItem('darkMode', 'true');
+
+	document.querySelector('body').classList.add('dark');
+	lightSwitch.src = 'images/design/MOON.svg';
+	mainLogo.src = 'images/design/dark_logo.png';
+}
+
+function turnOnLights() {
+	lightSwitch.setAttribute('onClick', 'javascript: turnOffLights();');
+	localStorage.setItem('darkMode', 'false');
+
+	lightSwitch.src = 'images/design/SUN.svg';
+	mainLogo.src = 'images/design/light_logo.png';
+	document.querySelector('body').classList.remove('dark');
+}
